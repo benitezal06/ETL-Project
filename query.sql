6aade1bb35dbf036399624f87bd9f565d2e7edcc
Your Name <mariko.falke@gmail.com>
2020-04-01 19:45:52 -0700

    Upload Query file

---
+++ query.sql
@@ -0,0 +1,18 @@
+-- Drop table if exists
+DROP TABLE population;
+
+-- Create a new table
+CREATE TABLE population (
+  COC_Number VARCHAR(10) NOT NULL,
+  Population DECIMAL
+);
+
+CREATE TABLE housing (
+	
+);
+
+CREATE TABLE homeless(
+
+);
+-- View table columns and datatypes
+SELECT * FROM population;
